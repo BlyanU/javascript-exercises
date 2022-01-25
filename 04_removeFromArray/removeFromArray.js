84e91d7838e06a8a02e8194c0c2d60f6442ee48c
BryanU <ungbryan01@gmail.com>
2022-01-24 18:55:02 -0800

    completed 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...theArgs) {
+  for (let i = 0; i < theArgs.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (theArgs[i] === arr[j]) {
+        arr.splice(j, 1);
+      }
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
