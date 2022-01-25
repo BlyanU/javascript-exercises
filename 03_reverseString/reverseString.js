6e070cf71c07035dc8cd089701a721e43b79427e
BryanU <ungbryan01@gmail.com>
2022-01-24 18:13:06 -0800

    completed 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
 const reverseString = function(str) {
+  let tmpString = '';
 
+  for (let i = str.length - 1; i >= 0; i--){
+    tmpString += str.charAt(i);
+  }
+  
+  return tmpString;
 };
 
 // Do not edit below this line
