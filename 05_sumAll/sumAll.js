f435e3e5234777ce811e0f5957dc11f4275d6297
BryanU <ungbryan01@gmail.com>
2022-01-25 10:32:23 -0800

    completed 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  let sum = 0;
+  let small;
+  let large;
 
+  if (a < 0 || b < 0) {
+    return 'ERROR';
+  } else if (typeof a != 'number' || typeof b != 'number') {
+    return 'ERROR';
+  }
+
+  if (a < b) {
+    small = a;
+    large = b;
+  } else {
+    small = b;
+    large = a;
+  }
+
+  for (let i = small; i <= large; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
