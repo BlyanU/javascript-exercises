8c38eba5256001e662045997abd492bb4bd19545
BryanU <ungbryan01@gmail.com>
2022-01-31 17:44:33 -0800

    completed 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+	for (const num of array) {
+    sum += num;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let count = 1;
+  for (const num of array) {
+    count *= num;
+  }
+  return count;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let product = 1;
+  if (num == 0) {
+    return 1;
+  }
+	for (let i = 1; i <= num; i++) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
