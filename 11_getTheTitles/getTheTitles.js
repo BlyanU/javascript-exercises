515dc31b09d7f32dc1ab948d820b09f19ba05f56
BryanU <ungbryan01@gmail.com>
2022-02-01 18:34:46 -0800

    completed 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+  const title = [];
+  for (const obj of array) {
+    title.push(obj.title);
+  }
+  return title;
 };
 
 // Do not edit below this line
