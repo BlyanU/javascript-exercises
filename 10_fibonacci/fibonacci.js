6bfafef2ac168f8bb255c5a51b92e554890dc94c
BryanU <ungbryan01@gmail.com>
2022-02-01 18:30:06 -0800

    completed 10_fionacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+  const seq = [];
+  if (index < 0) {
+    return 'OOPS';
+  }
+  for (let i = 0; i < index; i++) {
+    if (i == 0 || i == 1) {
+      seq.push(1);
+    } else {
+      seq.push(seq[i-2] + seq[i-1]);
+    }
+  } 
+  return seq[index - 1];
 };
 
 // Do not edit below this line
