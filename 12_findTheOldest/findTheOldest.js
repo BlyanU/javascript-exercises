2cf7b337e83761a68787cbdabd3d4915e122296d
BryanU <ungbryan01@gmail.com>
2022-02-01 23:31:09 -0800

    completed 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+  const sorted = array.sort((a,b) => {
+    let firstAge;
+    let secondAge;
+    const currYear = (new Date()).getFullYear();
+    if ("yearOfDeath" in a) {
+      firstAge = a.yearOfDeath - a.yearOfBirth;
+    } else {
+      firstAge = currYear - a.yearOfBirth;
+    }
 
+    if ("yearOfDeath" in b) {
+      secondAge = b.yearOfDeath - b.yearOfBirth;
+    } else {
+      secondAge = currYear - b.yearOfBirth;
+    }
+  
+    return (firstAge > secondAge) ? -1 : 1;
+  });
+
+  return array[0];
 };
 
 // Do not edit below this line
